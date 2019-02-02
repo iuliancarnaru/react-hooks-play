afcce815c392bde1c9b65fbc9e451d41a17e93bf
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-02-02 22:26:55 +0000

    learning more about useEffect

--- hooks-intro/src/AppFunction.js
+++ hooks-intro/src/AppFunction.js
@@ -1,23 +1,62 @@
-import React, { useState, useEffect, Fragment } from 'react'
+import React, { useState, useEffect, Fragment } from 'react';
+
+const initialLocationState = {
+    latitude: null,
+    longitude: null,
+    speed: null
+}
 
 const AppFunction = () => {
 
     const [count, setCount] = useState(0);
     const [isOn, setIsOn] = useState(false);
-    const [mousePosition, setMousePosition] = useState({x: null, y: null})
+    const [mousePosition, setMousePosition] = useState({x: null, y: null});
+    const [status, setStatus] = useState(navigator.onLine);
+    const [{latitude, longitude, speed}, setLocation] = useState(initialLocationState);
+
+    let mounted = true;
 
     // useEffect is triggered after each render
     // similar to componentDidMount
     useEffect(() => {
         document.title = `You have clicked ${count} times`;
         window.addEventListener('mousemove', handleMouseMove);
+        window.addEventListener('online', handleOnline);
+        window.addEventListener('offline', handleOffline);
+        navigator.geolocation.getCurrentPosition(handleGeolocation);
+        const watchId = navigator.geolocation.watchPosition(handleGeolocation);
 
 
         // similar to componentWillUnmount
         return () => {
             window.removeEventListener('mousemove', handleMouseMove);
+            window.removeEventListener('online', handleOnline);
+            window.removeEventListener('offline', handleOffline);
+            navigator.geolocation.clearWatch(watchId);
+            mounted = false;
         }
-    }, [count]);
+    }, 
+        // we specify any dependencies for sideEffect that will not be removed
+        [count]
+    );
+
+    const handleGeolocation = event => {
+        if(mounted) {
+            setLocation({
+                latitude: event.coords.latitude,
+                longitude: event.coords.longitude,
+                speed: event.coords.speed
+            })
+        }
+    }
+
+    const handleOnline = () => {
+        setStatus(true)
+    }
+
+    const handleOffline = () => {
+        setStatus(false)
+    }
 
     const handleMouseMove = event => {
         setMousePosition({
@@ -38,6 +77,8 @@ const AppFunction = () => {
         <Fragment>
             <h2>Counter Hooks</h2>
             <button onClick={incrementCount}>I was clicked {count} times</button>
+
+
             <h2>Toggle light</h2>
             <img
                 src={
@@ -51,9 +92,20 @@ const AppFunction = () => {
                 onClick={toggleLight}
                 alt="Flashlight">
             </img>
+
+
             <h2>Mouse Position</h2>
             {JSON.stringify(mousePosition, null, 2)}
             <br />
+
+
+            <h2>Network Status</h2>
+            <p>You are <strong>{ status ? 'online' : 'offline' }</strong></p>
+
+            <h2>Geolocation</h2>
+            <p>Latitude is: {latitude}</p>
+            <p>Longitude is: {longitude}</p>
+            <p>Your speed is: {speed ? speed : '0'}</p>
         </Fragment>
         
     )
