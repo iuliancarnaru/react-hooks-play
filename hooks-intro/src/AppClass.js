98c4efb1361f02ea8fe38ff0603193d05e9392dc
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-02-02 21:52:25 +0000

    componentDidMount vs useEffect

--- hooks-intro/src/AppClass.js
+++ hooks-intro/src/AppClass.js
@@ -1,22 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 
 class App extends Component {
   state = {
-    count: 0
+    count: 0,
+    isOn: false
+  }
+
+  componentDidMount() {
+    document.title = `You have clicked ${this.state.count} times`;
+  }
+
+  componentDidUpdate() {
+    document.title = `You have clicked ${this.state.count} times`;
   }
 
   incrementCount = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }))
+  }
+
+  toggleLight = () => {
+    this.setState(prevState => ({
+      isOn: !prevState.isOn
+    }))
   }
 
   render() {
     return (
-      <div>
+      <Fragment>
+          <h2>Counter Class</h2>
           <button onClick={this.incrementCount}>I was clicked {this.state.count} times</button>
-      </div>
+          <h2>Toggle light</h2>
+          <div
+            style={{
+              height: '50px',
+              width: '50px',
+              background: this.state.isOn ? 'yellow':'grey'
+            }}
+            onClick={this.toggleLight}>
+          </div>
+      </Fragment>
     );
   }
 }
--- hooks-intro/src/AppFunction.js
+++ hooks-intro/src/AppFunction.js
@@ -1,14 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 
 const AppFunction = () => {
 
     const [count, setCount] = useState(0);
+    const [isOn, setIsOn] = useState(false);
+
+    // useEffect is triggered after each render
+    useEffect(() => {
+        document.title = `You have clicked ${count} times`;
+    })
 
     const incrementCount = () => {
-        setCount(count + 1);
+        setCount(prevCount => prevCount + 1);
     }
 
-    return <button onClick={incrementCount}>I was clicked {count} times</button>
+    const toggleLight = () => {
+        setIsOn(prevIsOn => !prevIsOn)
+    }
+
+    return (
+        <Fragment>
+            <h2>Counter Hooks</h2>
+            <button onClick={incrementCount}>I was clicked {count} times</button>
+            <h2>Toggle light</h2>
+            <img
+                src={
+                    isOn 
+                        ? 'https://icon.now.sh/highlight/fd0'
+                        : 'https://icon.now.sh/highlight/aaa'}
+                style={{
+                height: '50px',
+                width: '50px',
+                }}
+                onClick={toggleLight}
+                alt="Flashlight">
+            </img>
+        </Fragment>
+        
+    )
 }
 
 export default AppFunction;
