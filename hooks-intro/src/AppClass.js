ac16ad77c4ae67e2f88f4e4e9566beffd267c15c
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-02-02 21:25:03 +0000

    useState hook

---
+++ hooks-intro/src/AppClass.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  state = {
+    count: 0
+  }
+
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  render() {
+    return (
+      <div>
+          <button onClick={this.incrementCount}>I was clicked {this.state.count} times</button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ hooks-intro/src/AppFunction.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const AppFunction = () => {
+
+    const [count, setCount] = useState(0);
+
+    const incrementCount = () => {
+        setCount(count + 1);
+    }
+
+    return <button onClick={incrementCount}>I was clicked {count} times</button>
+}
+
+export default AppFunction;
