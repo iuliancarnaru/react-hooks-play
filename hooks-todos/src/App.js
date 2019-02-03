93afb33db8ce21a870c268f12e5be72245be1364
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-02-03 16:33:27 +0000

    learning about useReducer

--- hooks-todos/src/App.js
+++ hooks-todos/src/App.js
@@ -1,12 +1,40 @@
-import React, { useContext } from 'react'
+import React, { useContext, useReducer } from 'react'
 import { UserContext } from './index';
 
+const initialState = {
+  count: 0
+}
+
+function reducer(state, action) {
+  switch(action.type){
+    case "increment":
+      return {
+        count: state.count + 1
+      };
+
+    case "decrement":
+      return {
+        count: state.count - 1
+      };
+
+    case "reset":
+      return initialState;
+
+    default:
+      return initialState
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
   const value = useContext(UserContext);
+
   return (
     <div>
-      Hello, {value}
+      Count: <strong>{state.count}</strong><br />
+      <button className="border p-1 m-1" onClick={() => dispatch({ type: "increment"})}>Increment</button>
+      <button className="border p-1 m-1" onClick={() => dispatch({ type: "decrement"})}>Decrement</button>
+      <button className="border p-1 m-1" onClick={() => dispatch({ type: "reset"})}>Reset</button>
     </div>
   )
 }
