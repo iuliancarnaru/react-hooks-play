5de5975efef48f7cac418522ff90d6e8a0c1e973
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-02-03 00:17:41 +0000

    learning about refs

--- hooks-news/src/App.js
+++ hooks-news/src/App.js
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 export default function App() {
 
   const [results, setResults] = useState([]);
-  const [query, setQuery] = useState('reacthooks')
+  const [query, setQuery] = useState('react hooks');
+  const searchInputRef = useRef();
 
   useEffect(() => {
     getDataFromApi();
-  },[query])
+  },[])
 
   // [] acts as component mount and unmount , not update (to run update insert upated value between [])
 
@@ -18,9 +19,27 @@ export default function App() {
     setResults(response.data.hits)
   }
 
+  const handleSearch = event => {
+    event.preventDefault();
+    getDataFromApi();
+  }
+
+  const handleClearSearch = () => {
+    setQuery("");
+    searchInputRef.current.focus();
+  }
+
+  // Refs give us a way to access the react elements created in the render method
+
   return(
     <>
-      <input type="text" onChange={event => setQuery(event.target.value)}/>
+      <form onSubmit={handleSearch}>
+      
+        <input type="text" onChange={event => setQuery(event.target.value)} value={query} ref={searchInputRef}/>
+        <button type="submit">Search</button>
+        <button type="button" onClick={handleClearSearch}>Clear</button>
+      </form>
+
       <ul style={{listStyle: 'none'}}>
       {results.map(result => (
         <li key={result.objectID}>
