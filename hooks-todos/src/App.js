e4e48d29d913d14ae2aa7f4e2b93dc40485dc55a
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-02-03 16:19:58 +0000

    learning about useContext

---
+++ hooks-todos/src/App.js
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react'
+import { UserContext } from './index';
+
+
+export default function App() {
+  const value = useContext(UserContext);
+  return (
+    <div>
+      Hello, {value}
+    </div>
+  )
+}
+
