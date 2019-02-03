23105160ddfc8f4037e2e211b417a29a10446e0a
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-02-03 00:41:56 +0000

    adding style, learning about tailwind css

--- hooks-news/src/App.js
+++ hooks-news/src/App.js
@@ -5,6 +5,9 @@ export default function App() {
 
   const [results, setResults] = useState([]);
   const [query, setQuery] = useState('react hooks');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null)
+
   const searchInputRef = useRef();
 
   useEffect(() => {
@@ -14,9 +17,17 @@ export default function App() {
   // [] acts as component mount and unmount , not update (to run update insert upated value between [])
 
   const getDataFromApi = async () => {
-    const response = await axios
-      .get(`http://hn.algolia.com/api/v1/search?query=${query}`);
-    setResults(response.data.hits)
+    setLoading(true);
+
+    try {
+      const response = await axios
+        .get(`http://hn.algolia.com/api/v1/search?query=${query}`);
+      setResults(response.data.hits)
+    } catch (error) {
+      setError(error)
+    }
+
+    setLoading(false);
   }
 
   const handleSearch = event => {
@@ -32,21 +43,27 @@ export default function App() {
   // Refs give us a way to access the react elements created in the render method
 
   return(
-    <>
-      <form onSubmit={handleSearch}>
+    <div className="container max-w-md mx-auto p-4 m-2 bg-purple-lightest shadow-lg rounded">
+    <img src="https://icon.now.sh/react/c00" alt="React Logo" className="float-right h-12"></img>
+    <h1 className="text-grey-darkest font-thin">Hooks news</h1>
+      <form className="mb-2" onSubmit={handleSearch}>
       
-        <input type="text" onChange={event => setQuery(event.target.value)} value={query} ref={searchInputRef}/>
-        <button type="submit">Search</button>
-        <button type="button" onClick={handleClearSearch}>Clear</button>
+        <input type="text" onChange={event => setQuery(event.target.value)} value={query} ref={searchInputRef} className="border p-1 rounded"/>
+        <button type="submit" className="bg-orange rounded m-1 p-1">Search</button>
+        <button type="button" className="bg-teal rounded m-1 p-1" onClick={handleClearSearch}>Clear</button>
       </form>
 
-      <ul style={{listStyle: 'none'}}>
+      {loading ? (
+        <div className="font-bold text-orange-dark">Loading results... Please be patient!</div>
+        ) : (<ul className="list-reset leading-normal">
       {results.map(result => (
         <li key={result.objectID}>
-          <a href={result.url}>{result.title}</a>
+          <a href={result.url} className="text-indigo-dark hover:text-indigo-darkest">{result.title}</a>
         </li>
       ))}
-      </ul>
-    </>
+      </ul>)}
+
+      { error && <div className="text-red font-bold">{error.message}</div>}
+    </div>
   )
 }
