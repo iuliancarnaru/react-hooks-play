f828bc889faacac7558567597f9628bae32ee268
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-02-03 00:06:04 +0000

    ading a search input

--- hooks-news/src/App.js
+++ hooks-news/src/App.js
@@ -4,19 +4,23 @@ import axios from 'axios';
 export default function App() {
 
   const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('reacthooks')
 
   useEffect(() => {
     getDataFromApi();
-  },[])
+  },[query])
+
+  // [] acts as component mount and unmount , not update (to run update insert upated value between [])
 
   const getDataFromApi = async () => {
     const response = await axios
-      .get(`http://hn.algolia.com/api/v1/search?query=reacthooks`);
+      .get(`http://hn.algolia.com/api/v1/search?query=${query}`);
     setResults(response.data.hits)
   }
 
   return(
     <>
+      <input type="text" onChange={event => setQuery(event.target.value)}/>
       <ul style={{listStyle: 'none'}}>
       {results.map(result => (
         <li key={result.objectID}>
